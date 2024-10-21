59d084de9d85d1247cb61f80bb5250163a3ccb2e
Josse <cooper17john@gmail.com>
2024-10-20 20:49:57 -0400

    LAB-05 added to questions

---
+++ src/hw03/q03.js
@@ -0,0 +1,27 @@
+const link = document.getElementById('myLink');
+link.href = 'https://keybr.com';
+link.textContent = 'Google';
+
+const buyList = [
+  "books",
+  "bread",
+  "eraser",
+  "earphones",
+  "collection-cards",
+  "hdmi",
+  "vga",
+  "motherboard",
+  "university-books",
+];
+
+const rootDiv = document.getElementById('root');
+
+const ul = document.createElement('ul');
+
+buyList.forEach(item => {
+  const li = document.createElement('li');
+  li.textContent = item;
+  ul.appendChild(li);
+});
+
+rootDiv.appendChild(ul);
